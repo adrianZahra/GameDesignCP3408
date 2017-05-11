b6aa8bde8f99447d19222ea7577b93a528f2b19c
adrianZahra <adrian.zahra@my.jcu.edu.au>
2017-05-11 21:48:48 +1000

    5th commit Adrian
    Made scripts to move the missle spwans with in relative location to the
    player

---
+++ CP3408Game/Assets/Scripts/Missiles/MoveMissleSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMissleSpawn : MonoBehaviour {
+    GameObject Player;
+    GameObject MissleSpawnPoint;
+    float position;
+
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        MissleSpawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MissleSpawnPoint.transform.position = new Vector3(Player.transform.position.x, 10, Player.transform.position.z); //moves spawn point acodingly to the players position
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+}
